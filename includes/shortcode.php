f6b1efa343a573af3cb160c01a25db0bf68ebca2
Julien Vernet <julien@vernet.me>
2016-04-05 11:32:20 +0700

    Basic Cloudinary integration. See #5

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -23,6 +23,11 @@ function wpscr_shortcode( $atts ) {
 	// https://pippinsplugins.com/retrieving-image-urls-of-galleries/
 	$gallery = get_post_gallery( $a['id'], false );
 
+	// Check if Cloudinary is enabled
+	$titan = TitanFramework::getInstance( 'wpscr_settings' );
+	$cloudinary_username = $titan->getOption( 'slider_cloudinary_account' );
+	$cloudinary_url = "http://res.cloudinary.com/$cloudinary_username/image/fetch/";
+
 	if( $gallery ) {
 	$attachments = explode( ",", $gallery['ids'] );
 	
@@ -31,7 +36,7 @@ function wpscr_shortcode( $atts ) {
 	
 			$link = wp_get_attachment_url( $attachment );
 			$size = wp_get_attachment_image_src( $attachment, 'full');
-			$slider .= "<div class='wpscr_slide'><img src='$link' width='$size[1]' height='$size[2]' alt=''></div>";
+			$slider .= "<div class='wpscr_slide'><img src='$cloudinary_url$link' width='$size[1]' height='$size[2]' alt=''></div>";
 	
 		}
 		$slider .= '</div>';
