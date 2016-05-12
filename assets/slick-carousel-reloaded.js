1db47d57f069a316312d236a24c85b64d6313199
Julien Vernet <julien@vernet.me>
2016-05-12 16:54:34 +0700

    Add additional conditions to avoid triggering JS errors

--- assets/slick-carousel-reloaded.js
+++ assets/slick-carousel-reloaded.js
@@ -1,27 +1,33 @@
 (function ($) {
 	"use strict";
 
-	$('.wpscr_slider').each(function (index, el) {
+	var selector = $('.wpscr_slider');
+	var condition = $.fn.slick && selector.length && typeof wpscr !== 'undefined' && wpscr !== null;
+	if (condition) {
 
-		// Get slider parameters based on its ID
-		var id = $(el).data('id');
-		var options = wpscr.sliders[id];
+		selector.each(function (index, el) {
 
-		// Initialiaze the slider
-		$(el).slick({
-			slide: '.wpscr_slide',
-			dots: options.dots,
-			infinite: options.infinite,
-			arrows: options.arrows,
-			speed: options.speed,
-			autoplay: options.autoplay,
-			fade: options.fade,
-			autoplaySpeed: options.autoplaySpeed,
-			slidesToShow: options.slidesToShow,
-			slidesToScroll: options.slidesToScroll,
-			adaptiveHeight: options.adaptiveHeight,
-			lazyLoad: options.lazyLoad
+			// Get slider parameters based on its ID
+			var id = $(el).data('id');
+			var options = wpscr.sliders[id];
+
+			// Initialiaze the slider
+			$(el).slick({
+				slide: '.wpscr_slide',
+				dots: options.dots,
+				infinite: options.infinite,
+				arrows: options.arrows,
+				speed: options.speed,
+				autoplay: options.autoplay,
+				fade: options.fade,
+				autoplaySpeed: options.autoplaySpeed,
+				slidesToShow: options.slidesToShow,
+				slidesToScroll: options.slidesToScroll,
+				adaptiveHeight: options.adaptiveHeight,
+				lazyLoad: options.lazyLoad
+			});
 		});
-	});
+
+	}
 
 }(jQuery));
