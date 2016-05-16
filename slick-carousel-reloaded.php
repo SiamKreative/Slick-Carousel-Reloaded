4ea6a95fbc7d3c59e0cc35629459b8de5db5be6d
Julien Vernet <julien@vernet.me>
2016-05-16 12:37:15 +0700

    Prevent PHP error if the plugin dependencies are missing

--- slick-carousel-reloaded.php
+++ slick-carousel-reloaded.php
@@ -30,19 +30,35 @@ if( ! defined( 'WPSCR_PLUGIN_BASENAME' ) ) {
 	define( 'WPSCR_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 }
 
-// Load composer dependencies individually (See https://git.io/vrG9d)
-require_once( 'vendor/gambitph/titan-framework/titan-framework-embedder.php' );
-require_once( 'vendor/johnbillion/extended-cpts/extended-cpts.php' );
+// Check if the plugin dependencies are installed
+if ( file_exists( WPSCR_PATH . 'vendor/autoload.php' ) ) {
 
-// Include required files
-require_once( 'includes/assets.php' );
-require_once( 'includes/custom_post_type.php' );
-require_once( 'includes/slider_options.php' );
-require_once( 'includes/metaboxes.php' );
-require_once( 'includes/settings_page.php' );
-require_once( 'includes/functions-misc.php' );
-require_once( 'includes/functions-slider.php' );
-require_once( 'includes/functions-resize.php' );
-require_once( 'includes/shortcode.php' );
-require_once( 'includes/dashboard_customisations.php' );
-require_once( 'includes/notices.php' );
+	// Load composer dependencies individually (See https://git.io/vrG9d)
+	require_once( 'vendor/gambitph/titan-framework/titan-framework-embedder.php' );
+	require_once( 'vendor/johnbillion/extended-cpts/extended-cpts.php' );
+
+	// Include required files
+	require_once( 'includes/assets.php' );
+	require_once( 'includes/custom_post_type.php' );
+	require_once( 'includes/slider_options.php' );
+	require_once( 'includes/metaboxes.php' );
+	require_once( 'includes/settings_page.php' );
+	require_once( 'includes/functions-misc.php' );
+	require_once( 'includes/functions-slider.php' );
+	require_once( 'includes/functions-resize.php' );
+	require_once( 'includes/shortcode.php' );
+	require_once( 'includes/dashboard_customisations.php' );
+
+} else {
+
+	// Add a warning notice if the plugin dependencies are missing
+	add_action( 'admin_notices', 'wpscr_missing_dependencies_warning' );
+	function wpscr_missing_dependencies_warning() { ?>
+
+		<div id="message" class="error">
+			<p><?php echo sprintf( wp_kses( __( 'The dependencies for Slick Carousel Reloaded are missing. Please <a href="%s">read this</a> for more details.', WPSCR_I18NDOMAIN ), array(  'a' => array( 'href' => array() ) ) ), esc_url( 'https://getcomposer.org/doc/01-basic-usage.md#installing-dependencies' ) ); ?></p>
+		</div>
+
+	<?php }
+
+}
