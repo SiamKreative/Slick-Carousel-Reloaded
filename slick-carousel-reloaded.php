9c9fa53f985c92da9e2cf1ed56da42ebbb4fc75e
Julien Vernet <julien@vernet.me>
2016-05-16 11:48:51 +0700

    Add comments and re-organize

--- slick-carousel-reloaded.php
+++ slick-carousel-reloaded.php
@@ -10,25 +10,24 @@
  * Author URI: https://siamkreative.com/
  */
 
-if( !defined('WPSCR_VERSION') ){
+// Setup plugin constants
+if( ! defined( 'WPSCR_VERSION' ) ){
 	define( 'WPSCR_VERSION', '1.0.0' );
 }
-
 define( 'WPSCR_URL', plugin_dir_url( __FILE__ ) );
 define( 'WPSCR_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'WPSCR_I18NDOMAIN', 'wpscr' );
 define( 'WPSCR_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
-// Load composer dependencies individually
+// Load composer dependencies individually (See https://github.com/SiamKreative/slick-carousel-reloaded/issues/14)
 require_once( 'vendor/gambitph/titan-framework/titan-framework-embedder.php' );
 require_once( 'vendor/johnbillion/extended-cpts/extended-cpts.php' );
 
+// Include required files
 require_once( 'includes/assets.php' );
 require_once( 'includes/custom_post_type.php' );
-
 require_once( 'includes/slider_options.php' );
 require_once( 'includes/metaboxes.php' );
-
 require_once( 'includes/settings_page.php' );
 require_once( 'includes/functions-misc.php' );
 require_once( 'includes/functions-slider.php' );
