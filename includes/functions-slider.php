SiamKreative/Slick-Carousel-Reloaded

commit 77849ef06dc43466679d78f2c8c1a2afcc58e1c9

Add helper function to get image optimized URL

includes/functions-slider.php
@@ -97,3 +97,36 @@ function wpscr_get_slider_image_size_name( $post_id ) {
 	return 'wpscr_size_' . $post_id;
 
 }
+
+/**
+ * Optimize an image using Cloudinary if the integration is setup
+ *
+ * @since 1.0.0
+ *
+ * @param string $url The image original URL
+ *
+ * @return string
+ */
+function wpsrc_get_image_optimized_url( $url ) {
+
+	$whitelist = array(
+		'127.0.0.1',
+		'::1'
+	);
+
+	// Don't use Cloudinary if the image is loaded from localhost (development environment)
+	if ( ! in_array( $_SERVER['REMOTE_ADDR'], $whitelist ) ) {
+		return $url;
+	}
+
+	// Get Cloudinary username
+	$titan    = TitanFramework::getInstance( 'wpscr_settings' );
+	$username = $titan->getOption( 'slider_cloudinary_account' );
+
+	if ( empty( $username ) ) {
+		return $url;
+	}
+
+	return sprintf( 'https://res.cloudinary.com/%1$s/image/fetch/%2$s', $username, $url );
+
+}

includes/shortcode.php
@@ -17,20 +17,6 @@ function wpscr_shortcode( $atts ) {
 	// https://pippinsplugins.com/retrieving-image-urls-of-galleries/
 	$gallery = get_post_gallery( $atts['id'], false );
 
-	// Cloudinary integration
-	$cloudinary_url = "";
-	$whitelist = array(
-		'127.0.0.1',
-		'::1'
-	);
-
-	// Disable if localhost
-	if( !in_array($_SERVER['REMOTE_ADDR'], $whitelist) ) {
-		$titan = TitanFramework::getInstance( 'wpscr_settings' );
-		$cloudinary_username = $titan->getOption( 'slider_cloudinary_account' );
-		$cloudinary_url = "https://res.cloudinary.com/$cloudinary_username/image/fetch/";
-	}
-
 	if( $gallery ) {
 	$attachments = explode( ",", $gallery['ids'] );
 	
@@ -38,13 +24,14 @@ function wpscr_shortcode( $atts ) {
 		foreach( $attachments as $attachment ) {
 
 			$media = wp_get_attachment_image_src( $attachment, 'full');
+			$image_url = wpsrc_get_image_optimized_url( $media[0] );
 
 			// Update markup for lazy load
 			if ($a['lazyload'] == 'ondemand' || $a['lazyload'] == 'progressive') {
 				// change the src attribute to data-lazy and add blank image
-				$image = "<img src='//www.gstatic.com/psa/static/1.gif' data-lazy='$cloudinary_url$media[0]' width='$media[1]' height='$media[2]' alt=''>";
+				$image = "<img src='//www.gstatic.com/psa/static/1.gif' data-lazy='$image_url' width='$media[1]' height='$media[2]' alt=''>";
 			} else {
-				$image = "<img src='$cloudinary_url$media[0]' {{layi}} width='$media[1]' height='$media[2]' alt=''>";
+				$image = "<img src='$image_url' {{layi}} width='$media[1]' height='$media[2]' alt=''>";
 			}
 
 			$slider .= "<div class='wpscr_slide'>$image</div>";

commit 527568cd052dfefa518763506e9858be6d5bc0f0

Fix localhost detection (bug was introduced with 77849ef06dc43466679d78f2c8c1a2afcc58e1c9). Closes #25

includes/functions-slider.php
@@ -115,7 +115,7 @@ function wpscr_get_image_optimized_url( $url ) {
 	);
 
 	// Don't use Cloudinary if the image is loaded from localhost (development environment)
-	if ( ! in_array( $_SERVER['REMOTE_ADDR'], $whitelist ) ) {
+	if ( in_array( $_SERVER['REMOTE_ADDR'], $whitelist ) ) {
 		return $url;
 	}
 